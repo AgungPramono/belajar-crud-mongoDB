6632f79f6f92c3b1bf1720bde8638bc11d997ee3
agung pramono <agungpermadi13@gmail.com>
2015-03-15 12:44:25 +0700

    Class entity

---
+++ src/main/java/com/agung/crud/mongodb/Person.java
@@ -0,0 +1,49 @@
+/*
+ * Silahkan digunakan dengan bebas / dimodifikasi
+ * Dengan tetap mencantumkan nama @author dan Referensi / Source
+ * Terima Kasih atas Kerjasamanya.
+ */
+package com.agung.crud.mongodb;
+
+/**
+ *
+ * @author Personal
+ */
+public class Person {
+   private String npm;
+   private String nama;
+   private String alamat;
+   private String phone;
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
